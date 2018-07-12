43c9be660bd895c6c550e7058dc5f3b7f6bd6f02
APPLE <apple@apples-macbook-pro-4.local>
2018-07-12 13:32:48 -0700

    convert to MVC design pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by APPLE on 12/07/18.
+//  Copyright © 2018 APPLE. All rights reserved.
+//
+
+import Foundation
+class Item  {
+    var title: String = ""
+    var done: Bool = false
+}
+
+
+
--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    var  itemArray = ["heart rate","stroke volume","cardiac output","HRV","intensity"]
+    
+    
+  var itemArray = [Item]()
+    
+    
+//    var  itemArray = ["heart rate","stroke volume","cardiac output","HRV","intensity","respiratory rate","total body water","oxgyen level"]
    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      if let  items = defaults.array(forKey: "TodoListArray") as? [String]
+        
+        let newItem = Item()
+        newItem.title = " Heart rate"
+//        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = " Heart rate variability"
+//        newItem2.done = true
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "cardiac output"
+//        newItem3.done = true
+        itemArray.append(newItem3)
+        
+        let newItem4 = Item()
+        newItem4.title = "stroke volume"
+//        newItem4.done = true
+        itemArray.append(newItem4)
+        
+        let newItem5 = Item()
+        newItem5.title = "respiratory rate"
+//        newItem5.done = true
+        itemArray.append(newItem5)
+        
+        let newItem6 = Item()
+        newItem6.title = "total body water"
+//        newItem6.done = true
+        itemArray.append(newItem6)
+        
+        let newItem7 = Item()
+        newItem7.title = "blood pressure"
+//        newItem7.done = true
+        itemArray.append(newItem7)
+        
+        let newItem8 = Item()
+        newItem8.title = "ECG"
+//        newItem8.done = true
+        itemArray.append(newItem8)
+        
+//        itemArray.append(newItem8)
+//        itemArray.append(newItem7)
+//        itemArray.append(newItem6)
+//        itemArray.append(newItem5)
+//        itemArray.append(newItem4)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem2)
+//        itemArray.append(newItem)
+//        itemArray.append(newItem8)
+//        itemArray.append(newItem7)
+//        itemArray.append(newItem6)
+//        itemArray.append(newItem5)
+//        itemArray.append(newItem4)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem2)
+//        itemArray.append(newItem)
+//
+        
+      if let  items = defaults.array(forKey: "TodoListArray") as? [Item]
       { itemArray = items
         }
         
@@ -28,8 +92,34 @@ class TodoListViewController: UITableViewController {
         return itemArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoIteamCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+//        print("cellForRowAtIndexpath called")
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "TodoItemCell")
+        
+     let cell  = tableView.dequeueReusableCell(withIdentifier: "TodoIteamCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+//        ternary operator ==>
+        //        valve = condition ? valveIfTrue : valveIfFalse
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+//        if item.done == true
+//        {
+//            cell.accessoryType = .checkmark
+//        }
+//        else{
+//            cell.accessoryType = .none
+//
+//
+//        }
+        
+        
+        
+        
+        
+        
 return cell
     }
 //    mark - tableView Delegate methods
@@ -37,12 +127,30 @@ return cell
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
 //        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        
+//      if  itemArray[indexPath.row].done == false
+//      {
+//        itemArray[indexPath.row].done = true
+//        }
+//      else{
+//        itemArray[indexPath.row].done == false
+//        }
+//
+        
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+//        {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+//
+        tableView.reloadData()
+        
+        
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -62,7 +170,10 @@ return cell
 //            print("add item pressed")
             print(textField.text)
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
